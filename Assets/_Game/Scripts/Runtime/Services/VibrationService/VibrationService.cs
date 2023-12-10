1db2323ba930ecf88030041508e18f818440081a
Berkay Ezdemir <berkayezdemir@hotmail.com>
2023-12-10 15:41:42 +0300

    haptic service added

--- Assets/_Game/Scripts/Runtime/Services/VibrationService/VibrationService.cs
+++ Assets/_Game/Scripts/Runtime/Services/VibrationService/VibrationService.cs
@@ -1,8 +1,21 @@
 ﻿
+using MoreMountains.NiceVibrations;
+using UnityEngine;
+
 public class VibrationService : Service, IVibrationService
 {
     public VibrationService(Contexts contexts) : base(contexts)
     {
-        
+    }
+
+    public void PlayHaptic(HapticTypes hapticType)
+    {
+        Debug.Log("Haptic Played: " + hapticType);
+        MMVibrationManager.Haptic(hapticType);
+    }
+
+    public void StopAllHaptics()
+    {
+        MMVibrationManager.StopAllHaptics();
     }
 }

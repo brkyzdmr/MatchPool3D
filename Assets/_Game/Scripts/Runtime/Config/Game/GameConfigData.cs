51ab616e5322e616bcfd661eeb282946f2103b37
Berkay Ezdemir <berkayezdemir@hotmail.com>
2023-12-11 16:57:27 +0300

    Update GameConfigData.cs

--- Assets/_Game/Scripts/Runtime/Config/Game/GameConfigData.cs
+++ Assets/_Game/Scripts/Runtime/Config/Game/GameConfigData.cs
@@ -4,9 +4,7 @@ public class GameConfigData
     public int goldPerStandardMerge;
     public int goldPerFinalMerge;
     public int goldPerLevelSecondsLeft;
-    public int objectDropIntervalSecondsMin;
-    public int objectDropIntervalSecondsMax;
-    
+
     public float objectsMaxSpeed;
     public float mergeVelocityThreshold;
     public float objectsMass;
